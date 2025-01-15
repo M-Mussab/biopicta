085f982389f5bdcdd0dfccf6964fdf7e764642a7
massbie <mmassab382@gmail.com>
2025-01-15 13:54:09 +0500

    last changes

--- src/app/sections/control.jsx
+++ src/app/sections/control.jsx
@@ -9,14 +9,14 @@ const testimonials = [
 const Control = () => {
   return (
     <>
-      <section className="bg-[#2A5650]">
+      <section className="bg-[#2A5650] pt-14 pb-14">
         <div className="flex">
-          <h1 className="text-green-600 text-2xl font-semibold items-center pt-10 pl-5">
+          <h1 className="text-green-600 text-3xl font-semibold items-center pt-10 pl-5">
             AI-powered Whizz<sup className="text-green-600 font-bold">TM</sup>
           </h1>
         </div>
         <div>
-          <h1 className="text-white text-2xl font-semibold items-center pb-8 pl-5">
+          <h1 className="text-white text-3xl font-semibold items-center pb-8 pl-5">
             Summaries your research findings
           </h1>
         </div>
@@ -24,7 +24,7 @@ const Control = () => {
           {testimonials.map((item, index) => (
             <div key={index} className="flex items-center gap-2">
               <div className="w-1.5 h-1.5 bg-white rounded-full"></div>
-              <h1 className="text-white text-md">{item}</h1>
+              <h1 className="text-white text-1xl">{item}</h1>
             </div>
           ))}
         </div>
